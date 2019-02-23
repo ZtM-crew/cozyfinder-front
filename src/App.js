2cda678c74c1bea784278fc723ccc1a090b8fc72
TamasKn <tamas.knisz@gmail.com>
2019-02-23 13:35:16 +0100

    Initial update

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+
+          {/** Run the application and check that appear the blue text in a blue box **/}
+          <section className="mw6 center shadow-3 b--solid bw1 br4 b--blue blue ma3">
+            <h1 className="tc code">React App with Tachyons</h1>
+          </section>
+          {/** If works properly, you can delete this block **/}
+
+        </div>
+    );
+  }
+}
+
+export default App;
