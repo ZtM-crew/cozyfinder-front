f249683b21a5f0045200b72d3320a526a3a337fe
TamasKn <tamas.knisz@gmail.com>
2019-04-26 15:19:53 +0200

    Map comp does not refresh GPS

--- src/App.js
+++ src/App.js
@@ -7,130 +7,153 @@ import Form from './components/Form/Form';
 import SearchBar from './components/SearchBar/SearchBar';
 import Map from './components/Map/Map';
 
-
-
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.handleLoginClick= this.handleLoginClick.bind(this);
-    this.handleLogoutClick= this.handleLogoutClick.bind(this);
-    this.handleSubmit= this.handleSubmit.bind(this);
+    constructor(props){
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.searchButton = this.searchButton.bind(this);
 
 
 
-    this.state = {
-      route: 'landing',
-      searchField: '',
+        this.state = {
+            route: 'landing',
+            searchField: '',
 
-      // Filter states
-      bed: 0,
-      bath: 0,
-      type: 'any',
-      garage: 'no',
-      //-----------
+            // Filter states
+            bed: 0,
+            bath: 0,
+            type: 'any',
+            garage: 'no',
+            //-----------
+
+            // Maps
+            latitude: 40.716906,
+            longitude: -73.988429
 
 
 
+        };
 
+        /** If Bed or Bath value = 0, means no additional filtering, input limited between 1 to 50 **/
+
+    }
+
+
+
+    handleLoginClick() {
+        this.setState({route: 'signin'});
+    }
+
+    handleLogoutClick() {
+        this.setState({route: 'landing'});
+    }
+
+    handleSubmit() {
+        this.setState({route: 'loggedin'});
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value})
+        //console.log(this.state)
+    };
+
+    bedInput = (e) => {
+        this.setState({ bed: e.target.value});
 
     };
 
-    // If Bed or Bath value = 0, means no additional filtering, input limited between 1 to 50
+    bathInput = (e) => {
+        this.setState({ bath: e.target.value});
+
+    };
+
+    typeChange = (e) => {
+        this.setState({type: e.target.value});
+
+    };
+
+    garageChange = (e) => {
+        this.setState({garage: e.target.value});
+
+    };
+
+    async searchButton() {
+
+        /** When the button clicked it back end sends all the neighbourhood data of New York. If any of the
+         * received name equal to the one in the searchfield, its GPS location will recorded in the State **/
+
+        const {searchField} = this.state;
+
+        const response = await fetch('http://localhost:3000/search/new york');
+        const data = await response.json();
 
 
-  }
+        for(let i=0; i<data['resultList'].length; i++){
+            if(searchField === data['resultList'][i]['name']['_text'].toLowerCase()) {
+                const lat = data['resultList'][i]['latitude']['_text']
+                const long = data['resultList'][i]['longitude']['_text']
 
-  handleLoginClick() {
-    this.setState({route: 'signin'});
-  }
-
-  handleLogoutClick() {
-    this.setState({route: 'landing'});
-  }
-
-  handleSubmit() {
-    this.setState({route: 'loggedin'});
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value})
-  };
-
-  bedInput = (e) => {
-    this.setState({ bed: e.target.value});
-
-  };
-
-  bathInput = (e) => {
-    this.setState({ bath: e.target.value});
-
-  };
-
-  typeChange = (e) => {
-    this.setState({type: e.target.value});
-
-  };
-
-  garageChange = (e) => {
-    this.setState({garage: e.target.value});
-
-  };
+                console.log(data['resultList'][i]['name']['_text'], lat, long)
 
 
-
-
-
-
-
-
-
-
-
-  render() {
-    const { route } = this.state;
-
-    let SEARCHBAR = <SearchBar searchChange={this.onSearchChange}
-                               bedInput={this.bedInput}
-                               bathInput={this.bathInput}
-                               typeChange = {this.typeChange}
-                               garageChange = {this.garageChange}
-    />;
-
-    let MAP = <Map />;
-
-    return (
-      <div>
-        <Title />
-
-
-
-        { route === 'landing'
-          ? <div>
-              <LogIn login={this.handleLoginClick} />
-              {SEARCHBAR}
-              {MAP}
-            </div>
-          : ( route === 'signin'
-              ? <Form submit={this.handleSubmit} />
-              : ( 
-                  <div>
-                      <Navbar logout={this.handleLogoutClick} />
-                      <Profile />
-                      {SEARCHBAR}
-                      {MAP}
-                   </div>
-
-                )
-            )
+                this.setState({latitude: lat})
+                this.setState({longitude: long})
+            }
+            //console.log(data['resultList'][i]['name']['_text'])     //listing all the New York neighbourhoods name
         }
+        console.log(this.state)
+
+    }
+
+
+    render() {
+        const { route, latitude, longitude } = this.state;
+
+        let SEARCHBAR = <SearchBar searchChange={this.onSearchChange}
+                                   bedInput={this.bedInput}
+                                   bathInput={this.bathInput}
+                                   typeChange = {this.typeChange}
+                                   garageChange = {this.garageChange}
+                                   searchButton = {this.searchButton}
+        />;
+
+        let MAP = <Map lat={latitude} long={longitude}/>;
+
+        return (
+            <div>
+                <Title />
 
 
 
-      </div>
-    );
-  }
-      
+                { route === 'landing'
+                    ? <div>
+                        <LogIn login={this.handleLoginClick} />
+                        {SEARCHBAR}
+                        {MAP}
+                    </div>
+                    : ( route === 'signin'
+                            ? <Form submit={this.handleSubmit} />
+                            : (
+                                <div>
+                                    <Navbar logout={this.handleLogoutClick} />
+                                    <Profile />
+                                    {SEARCHBAR}
+                                    {MAP}
+                                </div>
+
+                            )
+                    )
+                }
+
+
+
+            </div>
+        );
+    }
+
 }
 
 export default App;
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,15 +2,35 @@ import React from 'react';
 import './Map.css';
 
 
+/**
+ * Map API below. Its initial state GPS location is from the App.js state.
+ *
+ * Current bug: App updating the new GPS location to its state, Map state takes it but does not refresh the MAP component
+ * with the new data. Bug may be in the componentWillUpdate() function.
+ **/
+
 class Map extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            latitude: this.props.lat,
+            longitude: this.props.long
+        }
+
     }
+
+
 
     componentDidMount() {
 
+        let {latitude, longitude} = this.state
+
         const script = document.createElement('script');
+
+        // let lat = this.props.lat
+        // let long = this.props.long
 
         script.src = process.env.PUBLIC_URL + '/map_sdk/tomtom.min.js';
         document.body.appendChild(script);
@@ -20,14 +40,43 @@ class Map extends React.Component {
             window.tomtom.L.map('map', {
                 source: 'vector',
                 key: 'HepOBha6a6Qpm1KKkLvT88hpHEmOKSed',
-                center: [40.716906, -73.988429],
+                center: [latitude, longitude],
                 basePath: '/map_sdk',
-                zoom: 12
+                zoom: 16
 
             });
 
         }
+
+
     }
+
+    componentWillUpdate() {
+
+        let {latitude, longitude} = this.state
+
+        let lat = this.props.lat
+        let long = this.props.long
+
+
+        if(latitude != lat) {
+
+
+            this.setState({latitude: lat})
+            this.setState({longitude: long})
+
+            //setstate not working
+
+
+            console.log('map', this.state)
+            console.log('props', this.props.long, long)
+
+        }
+
+
+    }
+
+
 
     render() {
 
